b8becd2ee5749ae28f27d90281d9ffda6ad3f7cf
Rory Holmes <rorythomas511@gmail.com>
2024-03-23 13:09:51 +0000

    Create ExternalLink component

---
+++ app/components/ExternalLink.tsx
@@ -0,0 +1,24 @@
+import { MdArrowOutward } from 'react-icons/md';
+import Link from 'next/link';
+import styles from './ExternalLink.module.css';
+
+interface ExternalLinkProps {
+    href: string;
+    text: string;
+}
+
+const ExternalLink: React.FC<ExternalLinkProps> = ({ href, text }) => {
+    return (
+        <Link
+            href={href}
+            className={`w-30 p-1 pl-3 pr-2 py-2 text-md font-medium rounded-3xl border border-gray-100 ${styles.linkContainer} overflow-hidden`}
+        >
+            <div className="flex justify-between items-center mix-blend-difference">
+                <div>{text}</div>
+                <MdArrowOutward size={25} className={`${styles.arrow} ml-3`} />
+            </div>
+        </Link>
+    );
+};
+
+export default ExternalLink;
