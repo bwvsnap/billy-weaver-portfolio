88372b2189680a852d0af45d7dda72f537c8690d
Rory Holmes <rorythomas511@gmail.com>
2024-06-06 15:57:32 +0100

    adjust shop page font  tob be smaller on small screens

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,10 @@
 export default async function ShopPage() {
     return (
         <div className=" h-screen w-full flex flex-col items-center justify-center font-bold">
-            <h1 className=" font-monument text-6xl"> SHOP COMING SOON</h1>
+            <h1 className=" font-monument text-2xl md:text-6xl">
+                {' '}
+                SHOP COMING SOON
+            </h1>
         </div>
     );
 }
