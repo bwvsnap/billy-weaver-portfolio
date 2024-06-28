7c94832e15fb975d89d94bf383a0231bd4b1a34a
Rory Holmes <rorythomas511@gmail.com>
2024-06-28 14:16:49 +0100

    Remove Services section

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
                 <Hero />
                 <Intro />
                 <FeaturedWork />
-                <Services />
             </div>
         </>
     );
