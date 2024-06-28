5c849d2dffbb33f3766dd8b5520f6507fd203117
Rory Holmes <rorythomas511@gmail.com>
2024-06-28 15:04:25 +0100

    Add requested text to hero underneath h1

--- app/components/Sections/Hero.tsx
+++ app/components/Sections/Hero.tsx
@@ -12,11 +12,17 @@ const Hero = () => {
                     data-aos-once="true"
                     data-aos-duration="800"
                     data-aos-anchor="#test"
-                    className="my-auto mx-auto  "
+                    className="my-auto mx-auto space-y-10 "
                 >
-                    <h1 className="text-center font-monument font-bold text-5xl md:text-9xl 2xl:text-9xl text-stroke-hover transition-all duration-1000 ease-in cursor-default">
-                        BILLY
+                    <h1 className="text-center font-monument font-bold text-5xl md:text-8xl 2xl:text-9xl text-stroke-hover transition-all duration-1000 ease-in cursor-default">
+                        BILLY WEAVER
                     </h1>
+                    <h2 className="w-4/5 mx-auto text-center">
+                        Documentary-style photographer and videographer based in
+                        Manchester UK, blending classic techniques with
+                        experimentation to bring ideas and stories to life
+                        through compelling visual imagery.
+                    </h2>
                 </div>
             </div>
         </>
