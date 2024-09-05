5700b65ac73ca7e63294209b34b580edc601e68b
Rory Holmes <rorythomas511@gmail.com>
2024-09-05 14:49:24 +0100

    reduce gap between hero and intro text

--- app/components/Sections/Hero.tsx
+++ app/components/Sections/Hero.tsx
@@ -11,36 +11,31 @@ const Hero = () => {
     ];
 
     return (
-        <>
-            <div
-                id="test"
-                className="w-full h-screen flex flex-col pb-7 pt-[80px]"
-            >
-                <div className="my-auto mx-auto space-y-10 ">
-                    <div
-                        data-aos="zoom-out"
-                        data-aos-once="true"
-                        data-aos-duration="800"
-                        data-aos-anchor="#test"
-                    >
-                        <h1 className="text-center font-monument font-bold text-5xl md:text-8xl 2xl:text-9xl text-stroke-hover transition-all duration-1000 ease-in cursor-default">
-                            BILLY WEAVER
-                        </h1>
-                    </div>
-
-                    <h2
-                        data-aos="fade-in"
-                        data-aos-once="true"
-                        data-aos-duration="800"
-                        data-aos-delay="800"
-                        data-aos-anchor="#test"
-                        className="w-full font-monument font-bold md:w-4/5 mx-auto text-center text- md:text-3xl 2xl:text-4xl text-[#c8c8c8]"
-                    >
-                        PHOTOGRAPHER, VIDEOGRAPHER AND EDITOR
-                    </h2>
+        <div id="hero" className="w-full h-screen flex flex-col pt-[80px]">
+            <div className="my-auto mx-auto space-y-10 ">
+                <div
+                    data-aos="zoom-out"
+                    data-aos-once="true"
+                    data-aos-duration="800"
+                    data-aos-anchor="#test"
+                >
+                    <h1 className="text-center font-monument font-bold text-5xl md:text-8xl 2xl:text-9xl text-stroke-hover transition-all duration-1000 ease-in cursor-default">
+                        BILLY WEAVER
+                    </h1>
                 </div>
+
+                <h2
+                    data-aos="fade-in"
+                    data-aos-once="true"
+                    data-aos-duration="800"
+                    data-aos-delay="800"
+                    data-aos-anchor="#test"
+                    className="w-full font-monument font-bold md:w-4/5 mx-auto text-center text- md:text-3xl 2xl:text-4xl text-[#c8c8c8]"
+                >
+                    PHOTOGRAPHER, VIDEOGRAPHER AND EDITOR
+                </h2>
             </div>
-        </>
+        </div>
     );
 };
 
