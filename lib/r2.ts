a421cb4ef8e3b647d589f7c1d8e420f7fde8fe1c
Rory Holmes <rorythomas511@gmail.com>
2024-09-18 16:57:38 +0100

    Attempt to fix another vercel deployment bug

--- lib/r2.ts
+++ lib/r2.ts
@@ -5,8 +5,9 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
     endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-    s3ForcePathStyle: true, // needed for minio to work correctly
-    signatureVersion: 'v4'
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4',
+    region: 'auto'
 });
 
 // Function to list objects in the R2 bucket with a specific path
