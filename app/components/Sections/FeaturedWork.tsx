93d7a37d053defc0b5863578e2cf8bf56274c530
Rory Holmes <rorythomas511@gmail.com>
2024-09-20 18:57:32 +0100

    Fix featured work lightbox

--- app/components/Sections/FeaturedWork.tsx
+++ app/components/Sections/FeaturedWork.tsx
@@ -62,50 +62,51 @@ const FeaturedWork = () => {
     };
 
     return (
-        <div className="flex flex-col mb-32 justify-center items-center w-full space-y-14">
-            <h2
-                data-aos="fade-in"
-                data-aos-once="true"
-                data-aos-duration="800"
-                className="text-center font-monument text-2xl md:text-5xl lg:text-6xl 2xl:text-7xl  w-full font-bold"
-            >
-                FEATURED WORK
-            </h2>
-            <div className="w-full grid grid-cols-2 grid-flow-row md:grid-cols-3 md:grid-rows-2 gap-4 md:gap-5">
-                {images.map((image, index) => (
-                    <div
-                        key={index}
-                        className="relative rounded-2xl aspect-square overflow-hidden cursor-pointer group"
-                        data-aos="fade-in"
-                        data-aos-once="true"
-                        data-aos-duration="800"
-                        data-aos-delay={index * 100}
-                        onClick={() => handleImageClick(index)}
-                    >
-                        <Image
-                            className="transition-transform duration-500 group-hover:scale-110"
-                            src={image.src}
-                            alt="featured image"
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-500"></div>
-                    </div>
-                ))}
+        <>
+            <div className="flex flex-col mb-32 justify-center items-center w-full space-y-14">
+                <h2
+                    data-aos="fade-in"
+                    data-aos-once="true"
+                    data-aos-duration="800"
+                    className="text-center font-monument text-2xl md:text-5xl lg:text-6xl 2xl:text-7xl  w-full font-bold"
+                >
+                    FEATURED WORK
+                </h2>
+                <div className="w-full grid grid-cols-2 grid-flow-row md:grid-cols-3 md:grid-rows-2 gap-4 md:gap-5">
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className="relative rounded-2xl aspect-square overflow-hidden cursor-pointer group"
+                            data-aos="fade-in"
+                            data-aos-once="true"
+                            data-aos-duration="800"
+                            data-aos-delay={index * 100}
+                            onClick={() => handleImageClick(index)}
+                        >
+                            <Image
+                                className="transition-transform duration-500 group-hover:scale-110"
+                                src={image.src}
+                                alt="featured image"
+                                layout="fill"
+                                objectFit="cover"
+                            />
+                            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-500"></div>
+                        </div>
+                    ))}
+                </div>
+                <div
+                    data-aos="fade-in"
+                    data-aos-once="true"
+                    data-aos-duration="800"
+                    className="w-full flex flex-row  justify-center"
+                >
+                    <InternalLink
+                        href={'/portfolio'}
+                        text={'View All Work'}
+                        large={true}
+                    />
+                </div>
             </div>
-            <div
-                data-aos="fade-in"
-                data-aos-once="true"
-                data-aos-duration="800"
-                className="w-full flex flex-row  justify-center"
-            >
-                <InternalLink
-                    href={'/portfolio'}
-                    text={'View All Work'}
-                    large={true}
-                />
-            </div>
-
             {lightboxActive && (
                 <Lightbox
                     filteredMedia={images}
@@ -113,7 +114,7 @@ const FeaturedWork = () => {
                     onClick={closeLightbox}
                 />
             )}
-        </div>
+        </>
     );
 };
 
